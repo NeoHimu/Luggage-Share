e538ac9fce94571aacc1e7fb1ec37b91ab3a8fad
himanshu.singh <himanshu.singh@oneplus.com>
2019-12-13 18:06:36 +0530

    AWS storage added

--- main_project/settings.py
+++ main_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'tickets_posted.apps.TicketsPostedConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'storages',
     # django installed apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -140,8 +141,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'himusingh096@gmail.com'
-EMAIL_HOST_PASSWORD = 'neo123..'
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 
+
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+AWS_S3_SIGNATURE_VERSION='s3v4'
+AWS_S3_REGION_NAME='ap-south-1'
+
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
