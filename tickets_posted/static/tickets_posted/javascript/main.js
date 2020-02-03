2ea00529da55df9973f15a818fe37157efd62f84
himanshu.singh <himanshu.singh@oneplus.com>
2020-02-04 01:48:31 +0530

    brought the chat code to base.html

--- tickets_posted/static/tickets_posted/javascript/main.js
+++ tickets_posted/static/tickets_posted/javascript/main.js
@@ -13,8 +13,7 @@ const delay_by_in_ms = 100
 let scheduled_function = false
 
 const divCurrentUserAllTickets = $('#divCurrentUserAllTickets');
-
-
+const divAllDemandedUserTickets = $('#divAllDemandedUserTickets');
 
 $(document).ready(function(){
 			// Retrieve the object from storage
@@ -97,30 +96,49 @@ $('#arrival-airport').keyup(delay(function (e) {
 
 	}, 30)); //adds delay of 300 ms
 
-// user_input_arrival.on('keyup', function () {
 
-// })
+// demanded-users: it is the id of the <div> element in the base.html file where demandec_users.html file is included
+demanded_users.click(function(e) {
+		// console.log(e.target.id);
+		// button id is coded using ticket author id and ticket author username
+		// problem here is that anything other fields are not accessible from base.html
+		let str = e.target.id.split('-');
+		let other_userid = str[2];
+		let other_username = str[3];
+		// console.log($('#hidden-this-username').val());
+		// console.log($('#hidden-this-userid').val());
+		// console.log(other_userid);
+		// console.log(other_username);
 
+	openForm($('#hidden-this-userid').val(), other_userid, $('#hidden-this-username').val(), other_username);
 
-$("#currentUserTicketList li").click(function() {
-   			// console.log(this.id);
-   			// console.log($(this).find('#current-ticket-id').text());
-   			$.ajax({
-			url: '/find-all-matched-users/', //ajax call will be made to this url
-			type: 'get',
-			data:{
-				ticket_id:$(this).find('#current-user-ticket-id').text(),
-			},
-			dataType: "json",
-			success: function(response){
-				// alert("Successful!");
-				// console.log(response['html_from_view']);
-				demanded_users.html(response['html_from_view']);
-				divCurrentUserAllTickets.html("");
-
-			}
-		});
 });
+
+
+$("#currentUserTicketList li").click(function(e) {
+			//console.log(e.target.id);
+
+   			// only if button is clicked
+   			if(e.target.id === 'idProducerConsumer'){
+   				$.ajax({
+					url: '/find-all-matched-users/', //ajax call will be made to this url
+					type: 'get',
+					data:{
+						ticket_id:$(this).find('#hidden-current-user-ticket-id').text(),
+					},
+					dataType: "json",
+					success: function(response){
+						// alert("Successful!");
+						// console.log(response['html_from_view']);
+						demanded_users.html(response['html_from_view']);
+						divCurrentUserAllTickets.html("");
+						//console.log($('#divAllDemandedUserTickets').html());
+					}
+				});
+   			}
+   			
+});
+
 
 
 // Add event for selecting the ticket pdf
@@ -218,6 +236,112 @@ user_input_departure.on('keyup', function () {
 	scheduled_function = setTimeout(ajax_call_departure, delay_by_in_ms, endpoint_departure, request_parameters)
 })
 
+/////////////////////////// FIREBASE ///////////////////////////////////
 
+		const allRooms = firebase.database().ref().child('allRooms');
+	    const allChatsPerUser = firebase.database().ref().child('allChatsPerUser'); // all chats for each user id
+        // on page load, chat form should not appear. It should appear only on "Chat Now" clicked
+        document.getElementById("myForm").style.display = "none";
+        let roomName = "000";
+        let g_this_user = "";
+        let g_other_user = "";
+        let number_of_messages_to_load = 0;
+
+
+        $('#idCloseChatButton').on('click', closeForm);
+        $('#chat-message-submit').on('click', sendMessage);
+
+	    function openForm(first, second, this_user, other_user) {
+	    	number_of_messages_to_load = 30;
+	    	g_this_user = this_user;
+	    	g_other_user = other_user;
+	      // console.log(other_user);
+	      first = parseInt(first);
+	      second = parseInt(second);
+		  if(first < second)
+		  	roomName = first*1000000000000+second;
+		  else
+		  	roomName = second*1000000000000+first;
+		  console.log(roomName);
+		  document.getElementById("other-user").innerHTML = other_user;
+		  document.getElementById("myForm").style.display = "block";
+
+		    // load all previous messages
+		    let chat = document.getElementById('chat-log');
+		    let str = Object.assign("", chat.innerHTML);
+		    chat.innerHTML = "";
+		    // check if roomName exists na
+        	allRooms.child(roomName).orderByKey().limitToLast(number_of_messages_to_load).on('value', snapshot => {
+			    // console.log(snapshot.val());
+			    let str_temp="";
+			    console.log(number_of_messages_to_load);
+			    snapshot.forEach((messageSnapshot) => {
+			      // console.log(messageSnapshot.key);
+			      // console.log(messageSnapshot.val().message);
+			      // console.log("loop length");
+			      if(messageSnapshot.val().sender === this_user)
+			      	str_temp += '<div align="right"><span style="background: #96b5e0; border-color: #e1eaf7;  border-radius: 25px;">'+messageSnapshot.val().message+'</span></div>';
+			      else
+			      	str_temp += '<div align="left"><span style="border-color: #e1eaf7;  border-radius: 25px;">'+messageSnapshot.val().message+'</span></div>';
+			    });
+
+			    chat.innerHTML = str_temp;
+			    //scroll to bottom of the chat
+			    var e = document.getElementById('div-messages');
+				e.scrollTop = e.scrollHeight;
+			    // console.log(str_temp);
+		  });
+		}
+
+		function closeForm() {
+		  document.getElementById("myForm").style.display = "none";
+		} 
+		window.onload = function(){
+			closeForm();
+		} 
+
+		document.getElementById('chat-message-input').onkeyup = function(e) {
+            if (e.keyCode === 13) {  // enter, return
+                document.getElementById('chat-message-submit').click();
+                // console.log("pressed...");
+            }
+        };
+
+        function sendMessage(){
+        	console.log("Send message called");
+        	number_of_messages_to_load = 1;
+        	let time = new Date().getTime(); // in milliseconds
+        	let msg = document.getElementById("chat-message-input").value;
+        	document.getElementById("chat-message-input").value="";
+
+        	allRooms.child(roomName).push({
+        		sender: g_this_user,
+        		message: msg,
+        		time: time
+        	});
+        	console.log(g_other_user);
+        	allChatsPerUser.child(g_this_user).child(g_other_user).set({
+	        last_message: msg,
+	        room_id: roomName,
+	        last_message_timestamp: "12:34",
+	        last_seen: "4 minutes ago",
+	      });
+        }
+
+        $('#div-all-chats').click(function() {
+			// console.log("hello world!");
+			console.log($('#hidden-this-username').val());
+			console.log($('#hidden-this-userid').val());
+
+			allChatsPerUser.child($('#hidden-this-username').val()).orderByKey().limitToLast(10).on('value', snapshot => {
+			    // console.log(snapshot.val());
+			    snapshot.forEach((messageSnapshot) => {
+
+			    	let sender = messageSnapshot.key;
+			    	let last_message = messageSnapshot.val().last_message;
+			      	console.log(sender, last_message);
+			      });
+		    });
+		});
 
 });
